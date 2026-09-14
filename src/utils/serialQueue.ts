f07419e68328508675e988d289a7da5300a9e063
Orgitogj <orgitogjoci10@gmail.com>
2026-09-14 17:26:01 +0200

    feat: add serial write queue

---
+++ src/utils/serialQueue.ts
@@ -0,0 +1,17 @@
+export type SerialQueue = {
+  run<T>(task: () => Promise<T>): Promise<T>;
+};
+
+export function createSerialQueue(): SerialQueue {
+  let tail: Promise<void> = Promise.resolve();
+  return {
+    run<T>(task: () => Promise<T>): Promise<T> {
+      const result = tail.then(task);
+      tail = result.then(
+        () => undefined,
+        () => undefined,
+      );
+      return result;
+    },
+  };
+}
